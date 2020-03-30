fae3c8b8aa38285ffe9c048dc730081da90a82d7
clementdelbos <delbos.clement@gmail.com>
2020-03-30 12:25:15 +0200

    first commit

---
+++ src/main/java/com/udemy/javaee/GestionStockUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.javaee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionStockUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionStockUdemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemy/javaeetest/GestionStockUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.javaeetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionStockUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
